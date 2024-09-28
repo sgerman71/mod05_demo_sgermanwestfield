7ec0eaf97f1ffaa5e7cc2ab29507ceb72673d4ee
profe <professor@bryanmarshall.com>
2024-09-28 12:19:57 -0400

    update

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, g
 from .app_factory import create_app
+from .db_connect import close_db, get_db
 
 app = create_app()
 app.secret_key = 'your-secret'  # Replace with an environment variable
@@ -11,5 +12,12 @@ app.register_blueprint(runners)
 
 from . import routes
 
+@app.before_request
+def before_request():
+    g.db = get_db()
 
+# Setup database connection teardown
+@app.teardown_appcontext
+def teardown_db(exception=None):
+    close_db(exception)
 
